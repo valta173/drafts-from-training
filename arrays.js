925bb333203705faba9f4d1cc7df168cb3d50684
Valentin Taidonov <valta.code@gmail.com>
2022-12-17 16:58:59 +0700

    added the last 4 tasks
    Finished the topic about arrays

--- arrays.js
+++ arrays.js
@@ -97,6 +97,7 @@ console.log(reverse(cities));
 
 
 
+
 /* Реализуйте и экспортируйте по умолчанию функцию, которая высчитывает сумму всех элементов массива, которые делятся без остатка на 3 (три) */
 
 const coll = [];
@@ -531,3 +532,126 @@ console.log(isBracketStructureBalanced('<([])>'));
 console.log(isBracketStructureBalanced('<()][>'));
 console.log(isBracketStructureBalanced('<([)>'));
 console.log(isBracketStructureBalanced(''));
+
+
+
+
+/* Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход два отсортированных массива и находит их пересечение */
+
+const getIntersectionOfSortedArrays = (arr1, arr2) => {
+  let result = [];
+  for (const item of arr1) {
+    for (let i = 0; i < arr2.length; i++) {
+      if (item === arr2[i]) result.push(item);
+    }
+  }
+  return result;
+}
+
+export default getIntersectionOfSortedArrays();
+
+// Второй вариант (без вложенного цикла):
+
+const getIntersectionOfSortedArrays = (arr1, arr2) => {
+  let result = [];
+  for (const item of arr1) {
+    if (arr2.includes(item)) result.push(item);
+  }
+  return result;
+}
+
+console.log(getIntersectionOfSortedArrays([10, 11, 24], [10, 13, 14, 18, 24, 30]));
+console.log(getIntersectionOfSortedArrays([10, 11, 24], [-2, 3, 4]));
+console.log(getIntersectionOfSortedArrays([], [2]));
+
+
+
+
+/* Реализуйте и экспортируйте функцию getTheNearestLocation(), которая находит ближайшее место к указанной точке на карте и возвращает его. Параметры функции:
+
+locations – массив мест на карте. Каждое место - это массив из двух элементов, где первый элемент это название места, второй – точка на карте (массив из двух чисел x и y).
+point – текущая точка на карте. Массив из двух элементов-координат x и y.
+
+Для решения этой задачи деструктуризация не нужна, но мы хотим её потренировать. Поэтому решите эту задачу без обращения к индексам массивов.*/
+
+const locations = [
+  ['Park', [10, 5]],
+  ['Sea', [1, 3]],
+  ['Museum', [8, 4]],
+];
+
+
+export const getDistance = ([x1, y1], [x2, y2]) => {
+  const xs = x2 - x1;
+  const ys = y2 - y1;
+
+  return Math.sqrt(xs ** 2 + ys ** 2);
+}
+
+
+export const getTheNearestLocation = (locations, currentPoint) => {
+  if (locations.length === 0) {
+    return null;
+  }
+  
+  let result = locations[0];
+  let distance = 100;
+  
+  for (const item of locations) {
+    const [x1,y1] = item[1];
+    const [x2,y2] = currentPoint;
+    
+    if (getDistance([x1, y1], [x2, y2]) < distance ) {
+      distance = getDistance([x1, y1], [x2, y2]);
+      result = item;
+    }
+  }
+  return result;
+}
+
+console.log(getTheNearestLocation(locations, [10, 10]));
+console.log(getTheNearestLocation(locations, [1, 1]));
+console.log(getTheNearestLocation(locations, [5, 5]));
+
+
+
+
+/* Реализуйте и экспортируйте функцию getMax(), которая ищет в массиве максимальное значение и возвращает его */
+
+export const getMax = arr => {
+  if (arr.length === 0) return [];
+  
+  let max = arr[0];
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
+
+console.log(getMax([1, 10, 8]));
+console.log(getMax([25, 10, 8]));
+console.log(getMax([1, 10, 53]));
+console.log(getMax([1, 143, 8]));
+
+
+
+
+/* Реализуйте и экспортируйте функцию flatten(). Эта функция принимает на вход массив и выпрямляет его: если элементами массива являются массивы, то flatten сводит всё к одному массиву, раскрывая один уровень вложенности.
+
+В js эта функция реализована как метод flat() у массивов. Его использовать нельзя. */
+
+export const flatten = arr => {
+  let result = [];
+  for (const item of arr) {
+    if (Array.isArray(item))
+      result.push(...item);
+    else 
+      result.push(item);
+  }
+  return result;
+}
+
+console.log(flatten([[1], [3, 2], 9]));
+console.log(flatten([]));
+console.log(flatten([1, [[2], [3]], [9]]));
