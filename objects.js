58501f36de1351bd323f686e0b6730f78e90bd42
Valentin Taidonov <valta.code@gmail.com>
2022-12-21 10:20:49 +0700

    added f() that extracts a value from an object
    get() that extracts a value from an object of any nesting depth using the specified keys

--- objects.js
+++ objects.js
@@ -121,3 +121,45 @@ export default pick();
 console.log(pick(data, ['none', 'cores']));
 console.log(pick(data, ['user', 'os']));
 console.log(pick(data, []));
+
+
+
+
+/*Реализуйте и экспортируйте по умолчанию функцию, которая извлекает из объекта любой глубины вложенности значение по указанным ключам. Параметры:
+
+Исходный объект
+Цепочка ключей (массив), по которой ведётся поиск значения
+В случае, когда добраться до значения невозможно, возвращается null.
+
+В этой задаче нельзя использовать библиотеку lodash. Смысл задачи в том, чтобы реализовать всё самостоятельно.*/
+
+const data = {
+  user: 'ubuntu',
+  hosts: {
+    0: {
+      name: 'web1',
+    },
+    1: {
+      name: 'web2',
+      null: 3,
+      active: false,
+    },
+  },
+};
+
+const get = (obj, arr) => {
+  let value = obj;
+  for (const key of arr) {
+    value = value[key];
+    if (value === undefined) return null;
+  }
+  return value;
+};
+
+console.log(get(data, ['undefined'])); // null
+console.log(get(data, ['user'])); // 'ubuntu'
+console.log(get(data, ['user', 'ubuntu'])); // null
+console.log(get(data, ['hosts', 1, 'name'])); // 'web2'
+console.log(get(data, ['hosts', 0])); // { name: 'web1' }
+console.log(get(data, ['hosts', 1, null])); // 3
+console.log(get(data, ['hosts', 1, 'active'])); // false
